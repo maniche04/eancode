541c8d7655e84c55e49f5f81e373f7e8ef041ca3
santosh <santosh@JIH-114-141.jizanperfumes.com>
2016-01-20 18:36:42 +0400

    changed the item inputbox

--- src/eancode/TableRowResizer.java
+++ src/eancode/TableRowResizer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package whusers;
+package eancode;
 import javax.swing.*;
 import javax.swing.table.*;
 import javax.swing.event.MouseInputAdapter;
--- src/eancode/TableRowResizerSeltbl.java
+++ src/eancode/TableRowResizerSeltbl.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package whusers;
+package eancode;
 
 import java.awt.Cursor;
 import java.awt.Point;
