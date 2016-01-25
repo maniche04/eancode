134efd3611738b6f94270c44208bfa607872fb86
santosh <santosh@JIH-114-141.jizanperfumes.com>
2016-01-25 18:19:55 +0400

    changed the item inputbox

--- src/eancode/WelcomeLoginScreen.java
+++ src/eancode/WelcomeLoginScreen.java
@@ -236,40 +236,35 @@ public void infouserType() throws Exception
     
 }
   
-    
+
     private void loginbtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_loginbtnActionPerformed
-     // TODO add your handling code here:
-        
+     //TODO add your handling code here:
      usernm = usrid.getText();
      passwrd = pass.getText();
      gv.setGlobusername(usernm);
      gv.setGlobpassword(passwrd);
-            
-            try {
-                
+            try{
                 pp.sendPost();
-                if (gv.getPassFail() == 1) { 
-                    
-                  EanHome eanscannerobj =  new EanHome();
-                  eanscannerobj.setVisible(true);
-                  this.setVisible(false);
-                  infouserType();
+                if (gv.getPassFail() == 1){   
+                EanHome eanscannerobj =  new EanHome();
+                eanscannerobj.setVisible(true);
+                this.setVisible(false);
+                infouserType();
                 } 
-                else
+            else
                 {
-                    JOptionPane.showMessageDialog(this, "Invalid UserName/Password!");  
+            JOptionPane.showMessageDialog(this, "Invalid UserName/Password!");  
                 }
-               
-            } catch (Exception ex) {
+            }catch (Exception ex) {
                 Logger.getLogger(WelcomeLoginScreen.class.getName()).log(Level.SEVERE, null, ex);
-            }
-              
+            }    
     }//GEN-LAST:event_loginbtnActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
+/**
+  *@param args the command line arguments
+  */
+    
+public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -294,7 +289,7 @@ public void infouserType() throws Exception
         }
         //</editor-fold>
 
-        /* Create and display the form */
+        /*Create and display the form*/
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new WelcomeLoginScreen().setVisible(true);
